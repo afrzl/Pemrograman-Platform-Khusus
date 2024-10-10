2eda7ff45676baa4ff63a57d99ee6a13a30072a2
Muh. Nur Afrizal <muhamadnurafrizal5@gmail.com>
2024-10-10 08:48:29 +0700

    pertemuan 3

---
+++ Pertemuan 3/soap/src/main/java/org/example/soap/CountryEndpoint.java
@@ -0,0 +1,28 @@
+package org.example.soap;
+
+import com.example.soap.gen.GetCountryRequest;
+import com.example.soap.gen.GetCountryResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class CountryEndpoint {
+    private static final String NAMESPACE_URI = "http://www.example.com/soap/gen";
+    private CountryRepository countryRepository;
+
+    @Autowired
+    public CountryEndpoint(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+    @ResponsePayload
+    public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+        GetCountryResponse response = new GetCountryResponse();
+        response.setCountry(countryRepository.findCountry(request.getName()));
+        return response;
+    }
+}
